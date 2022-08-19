6d1573a6b9d1b01f839d937e0f7a12a831187a80
Yuan Si <do4suki@gmail.com>
2022-08-19 19:58:28 +0800

    patcher: Add patchset for tm-release
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I1c06cdc12d513987560a7b5431de31d1bba4a0f6

--- patcher.sh
+++ patcher.sh
@@ -2,6 +2,18 @@
 
 ROOT="${PWD}"
 REPOS=(
+    'bionic'
+    'bootable/recovery'
+    'build/soong'
+    'external/tinycompress'
+    'frameworks/av'
+    'frameworks/native'
+    'hardware/libhardware'
+    'vendor/qcom/opensource/data-ipa-cfg-mgr'
+    'vendor/qcom/opensource/dataservices'
+    'vendor/qcom/opensource/power'
+    'vendor/qcom/opensource/usb'
+    'vendor/qcom/opensource/vibrator'
 )
 
 for repo in "${REPOS[@]}"; do
